2d23e8d3298a355dcdf96dd621d88f08592ba1c1
Shivang Dave <shivangdave@shivangs-macbook-pro.local>
2018-07-04 21:37:56 -0500

    Initial Commit

---
+++ Automatic Backup WiFi Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Automatic Backup WiFi Camera
+//
+//  Created by Shivang Dave on 7/4/18.
+//  Copyright © 2018 Shivang Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
