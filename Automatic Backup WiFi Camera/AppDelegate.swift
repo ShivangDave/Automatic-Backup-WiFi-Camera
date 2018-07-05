a31ad30aa890d871369fdd52f66619355b1cd9ef
Shivang Dave <daveshivang15@gmail.com>
2018-07-04 22:48:01 -0500

    Enable push notifications

--- Automatic Backup WiFi Camera/AppDelegate.swift
+++ Automatic Backup WiFi Camera/AppDelegate.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let defaults = UserDefaults.standard
+    var token : String?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
         
+        UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]){ (granted, error) in }
+        application.registerForRemoteNotifications()
+        
         return true
     }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)
+    {
+        token = deviceToken.hexString
+        defaults.set(token, forKey: "token")
+        print(token!)
+    }
 
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void)
+    {
+        let aps = userInfo["aps"] as! [String : AnyObject]
+        print(aps)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication)
     {
         
---
+++ Automatic Backup WiFi Camera/Constant.swift
@@ -0,0 +1,96 @@
+//
+//  Constant.swift
+//  Automatic Backup WiFi Camera
+//
+//  Created by Shivang Dave on 7/4/18.
+//  Copyright © 2018 Shivang Dave. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MaterialComponents
+import Alamofire
+
+let activityIndicator = MDCActivityIndicator()
+let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+let primaryColor = UIColor(red: 228.0/255, green: 46.0/255, blue: 106.0/255, alpha: 1.0)
+let primaryFont = UIFont(name: "Rubik-Medium", size: 20.0)!
+let primaryFontAlert = UIFont(name: "Rubik-Medium", size: 12.0)!
+
+extension UIViewController
+{
+    func showSnack(_ message: String, _ buttonTitle:String)
+    {
+        let message = MDCSnackbarMessage(text: message)
+        
+        let action = MDCSnackbarMessageAction()
+        let handler = {() in
+            let answereMessage = MDCSnackbarMessage(text: "Done")
+            MDCSnackbarManager.show(answereMessage)
+        }
+        action.handler = handler
+        action.title = buttonTitle
+        message.action = action
+        MDCSnackbarManager.show(message)
+    }
+    
+    
+    func presentIt(_ vc : String)
+    {
+        let dvc = mainStoryboard.instantiateViewController(withIdentifier: vc)
+        dvc.modalPresentationStyle = .custom
+        dvc.modalTransitionStyle = .crossDissolve
+        present(dvc, animated: true, completion: nil)
+    }
+    
+    func startAnimating(_ activityIndicator: MDCActivityIndicator)
+    {
+        activityIndicator.sizeToFit()
+        activityIndicator.indicatorMode = .indeterminate
+        activityIndicator.center = self.view.center
+        view.addSubview(activityIndicator)
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func stopAnimating(_ activityIndicator: MDCActivityIndicator)
+    {
+        activityIndicator.stopAnimating()
+    }
+    
+    func sendReq(_ data:Parameters,_ url:String)->(String,Bool)
+    {
+        var response = ""
+        var success = false
+        
+        Alamofire.request(url, method: .post, parameters: data, encoding: JSONEncoding.default).responseJSON{(res) in
+            switch res.result
+            {
+            case .success(let json):
+                let dic = json as! NSDictionary
+                let code = dic.value(forKey: "response") as! String
+                response = code
+                success = true
+                //print(code)
+                //self.showAlert("Response",code)
+                
+                
+            case .failure(let error):
+                response = error.localizedDescription
+                success = false
+                //print(error.localizedDescription)
+                //self.showAlert("Response",error.localizedDescription)
+            }
+        }
+        return (response,success)
+    }
+}
+
+extension Data
+{
+    var hexString: String
+    {
+        let hexString = map { String(format: "%02.2hhx", $0) }.joined()
+        return hexString
+    }
+}
